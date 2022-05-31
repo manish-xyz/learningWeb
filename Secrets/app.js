539af076be03a136fcb9d40b0b0aacc30b1ad33b
Manish <92672117+manish-xyz@users.noreply.github.com>
2022-05-31 16:40:35 +0530

    Added google auth

--- Secrets/app.js
+++ Secrets/app.js
@@ -1,20 +1,26 @@
 require('dotenv').config();
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
-const app = express();
 const mongoose = require('mongoose');
 const session = require('express-session');
 const passport = require('passport');
 const passportLocal = require('passport-local-mongoose');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const findOrCreate = require('mongoose-findorcreate');
 
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.set('view engine', 'ejs');
+
+// -- Creating new Session --------------------------------
 
 app.use(session({
-    secret: "Hello bro",
+    secret: 'Hello bro',
     resave: false,
     saveUninitialized: false
 }));
@@ -22,27 +28,73 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+// -- conncet database --------------------------------
+
 mongoose.connect('mongodb://localhost:27017/usersDB');
 
-const PORT = process.env.PORT || 3000;
+// --  Creating user --------------------------------
 
 const userSchema = new mongoose.Schema({
-    email: 'string',
-    password: 'string'
+    email: String,
+    password: String,
+    googleId: String,
 });
 
 userSchema.plugin(passportLocal);
+userSchema.plugin(findOrCreate);
 
 const User = mongoose.model('User', userSchema);
 
+// -- --------------------------------
+
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser( (user, done) => {
+
+    done(null, user._id);
+});
+
+passport.deserializeUser( (id, done) => {
+
+  User.findById(id, (err, user) => {
+    done(err, user);
+  });
+});
+
+// -- Google open auth --------------------------------
+
+passport.use(new GoogleStrategy({
+
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: 'http://localhost:3000/auth/google/testing',
+    userProfileURL: 'https://www.googleapis.com/oauth2/v3/userinfo',
+},
+     (accessToken, refreshToken, profile, cb) => {
+
+        User.findOrCreate({ googleId: profile.id }, function (err, user) {
+            return cb(err, user);
+        });
+    }
+));
+
+// ---- GET  Routes -----
 
 app.get('/', (req, res) => {
     res.render('home');
 });
+
+app.get('/auth/google', 
+   passport.authenticate('google', { scope: ['profile'] })
+);
+
+app.get('/auth/google/testing', 
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  (req, res) => {
+    // Successful authentication, redirect home.
+    console.log(profile);
+    res.redirect('/secrets');
+  });
 
 app.get('/login', (req, res) => {
     res.render('login');
@@ -72,6 +124,8 @@ app.get('/logout', (req, res) => {
     res.redirect('/');
 })
 
+// ---- POST  Routes -----
+
 app.post('/register', (req, res) => {
 
     User.register({ username: req.body.username }, req.body.password, (err, user) => {
@@ -84,7 +138,7 @@ app.post('/register', (req, res) => {
         else {
 
             passport.authenticate("local")(req, res, () => {
-            res.redirect('/secrets');
+                res.redirect('/secrets');
             })
         }
     })
@@ -92,24 +146,26 @@ app.post('/register', (req, res) => {
 
 app.post('/login', (req, res) => {
 
-    const user = new User ({
+    const user = new User({
         username: req.body.username,
         password: req.body.password
     });
 
     req.login(user, (err) => {
 
-        if(err){
+        if (err) {
 
             console.log(err);
         } else {
 
-            passport.authenticate("local")(req, res, () => {
+            passport.authenticate('local')(req, res, () => {
                 res.redirect('/secrets');
             });
         }
     });
 });
+
+// ---- 
 
 app.listen(PORT, () => {
 
